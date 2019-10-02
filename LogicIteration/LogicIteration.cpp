76fbbe74d3b6e1d57e1f35f92cd3c1ebaae3f22a
Jake Hall <jake-thomas-hall@hotmail.co.uk>
2019-10-02 14:27:48 +0100

    Initial Logic branch commit

---
+++ LogicIteration/LogicIteration.cpp
@@ -0,0 +1,38 @@
+// LogicIteration.cpp : Demo of logic and iteration within C++
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+#include <conio.h>
+
+int main()
+{
+	int iChoice = 0;
+
+	std::cout << "1: Login\n";
+	std::cout << "2: Exit\n";
+
+	std::cout << "Please make a selection: ";
+	std::cin >> iChoice;
+
+	switch (iChoice) 
+	{
+		case 1: 
+		{
+
+		}
+		case 2: 
+		{
+
+		}
+		default: 
+		{
+
+		}
+	}
+	
+	return 0;
+}
+
