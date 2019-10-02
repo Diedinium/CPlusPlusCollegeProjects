2bc415e06aa726ccc99500607823d59852d62a0c
Jake Hall <jake-thomas-hall@hotmail.co.uk>
2019-10-02 14:35:55 +0100

    menu and login

--- LogicIteration/LogicIteration.cpp
+++ LogicIteration/LogicIteration.cpp
@@ -10,6 +10,10 @@
 int main()
 {
 	int iChoice = 0;
+	std::string sUsername = "Jake";
+	std::string sPassword = "password";
+	std::string sUsernameInput;
+	std::string sPasswordInput;
 
 	std::cout << "1: Login\n";
 	std::cout << "2: Exit\n";
@@ -21,15 +25,31 @@ int main()
 	{
 		case 1: 
 		{
+			std::cout << "Welcome to the login screen\n";
 
+			std::cout << "Username : ";
+			std::cin >> sUsernameInput;
+			
+			std::cout << "Password : ";
+			std::cin >> sPasswordInput;
+
+			if ((sUsername == sUsernameInput) && (sPassword == sPasswordInput)) {
+				std::cout << "You are now logged in.\n";
+				system("pause");
+			}
+			else {
+				std::cout << "Username or password incorrect.\n";
+			}
 		}
 		case 2: 
 		{
-
+			std::cout << "Logging you out and exiting...\n";
+			system("Pause");
+			system("exit");
 		}
 		default: 
 		{
-
+			std::cout << "Not recognised input, please try again.\n";
 		}
 	}
 	
