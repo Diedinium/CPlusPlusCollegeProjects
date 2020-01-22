125d6786216c207ac6596028f43c2d35483ab96b
Jake Hall <ws326200@weston.ac.uk>
2020-01-22 12:24:08 +0000

    Adding a recap progject.

--- ForLoopPowerOf10/ForLoopPowerOf10.cpp
+++ ForLoopPowerOf10/ForLoopPowerOf10.cpp
@@ -2,20 +2,30 @@
 
 #include <iostream>
 
+// Function that takes the starting value, and exponent
+int power(int x, int y) {
+	// Set starting power.
+	int power = 1;
+	// Return 1 if y is 0
+	if (y == 0) {
+		return 1;
+	}
+	// Loop until i is equal to the exponent (y)
+	for (int i = 1; i <= y; i++) {
+		// Multiply power by x
+		power *= x;
+	}
+	return power;
+}
+
 int main()
 {
-	int iNumber = 0;
+	int x = 5;
+	int y = 10;
+	std::cout << "x = " << x << std::endl;
+	std::cout << "y = " << y << std::endl;
 
-	std::cout << "Please specify a number : ";
-	std::cin >> iNumber;
-
-	int iTotal = iNumber;
-
-	for (int i = 0; i < 10; i++) {
-		iTotal *= iNumber;
-	}
-
-	std::cout << iNumber << " to the power of 10 : " << iTotal << std::endl;
+	std::cout << "x^y = " << power(x, y) << std::endl;
 
 	return 0;
 }
