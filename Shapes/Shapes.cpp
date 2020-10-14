5cefc0c39f8d4fb30a83de3fe0de84ed2390cfac
Jake Hall <ws326200@weston.ac.uk>
2020-10-14 11:37:46 +0100

    Started work on shapes task.

---
+++ Shapes/Shapes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include "Shapes.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ Shapes/Shapes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#ifndef SHAPES_H
+#define SHAPES_H
+
+class Shape {
+
+};
+
+#endif // SHAPES_H
