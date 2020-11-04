3551e9d5219c2949fdd55b25ae305f52e98dfc06
Jake Hall <jake-thomas-hall@hotmail.co.uk>
2020-11-04 11:52:25 +0000

    Walls class added.

--- MultipleInheritance/MultipleInheritance.cpp
+++ MultipleInheritance/MultipleInheritance.cpp
@@ -26,20 +26,36 @@ public:
     void SetTileSize(double tileSize) { _dTileSize = tileSize; }
 };
 
-class Room : public Dimensions, public Tiles {
+class Walls {
+protected:
+    double _dWallHeight = 10;
+    double _dWallpaperSize = 2;
+    double _dWallpaperCost = 1;
+    double _dWallArea;
+public:
+    double GetWallArea() { return _dWallArea; }
+};
+
+class Room : public Dimensions, public Tiles, public Walls {
 private:
     double _dArea;
     double _dTilesRequired;
+    double _dWallpaperRequired;
     double _dFloorPrice;
+    double _dWallPrice;
 public:
     Room();
     void CalculateArea();
     void CalculateTilesRequired();
     void CalculateFloorPrice();
+    void CalculateWallPrice();
 
     double GetArea() { return _dArea; }
     double GetTilesRequired() { return _dTilesRequired; }
     double GetFloorPrice() { return _dFloorPrice; }
+
+    double GetWallpaperRequired() { return _dWallpaperRequired; }
+    double GetWallPrice() { return _dWallPrice; }
 };
 
 int main()
@@ -59,18 +75,28 @@ int main()
     objRoom.CalculateArea();
     objRoom.CalculateTilesRequired();
     objRoom.CalculateFloorPrice();
+    objRoom.CalculateWallPrice();
 
     std::cout << "\n";
 
     std::cout << "Floor Area: " << objRoom.GetArea() << "\n";
     std::cout << "Tiles Required: " << objRoom.GetTilesRequired() << "\n";
     std::cout << "Floor Price: " << objRoom.GetFloorPrice() << "\n";
+
+    std::cout << "\n";
+
+    std::cout << "Wall Area: " << objRoom.GetWallArea() << "\n";
+    std::cout << "Wallpaper Required: " << objRoom.GetWallpaperRequired() << "\n";
+    std::cout << "Wall Price: " << objRoom.GetWallPrice() << "\n";
 }
 
 Room::Room() {
     _dArea = 0;
     _dTilesRequired = 0;
     _dFloorPrice = 0;
+    _dWallArea = 0;
+    _dWallPrice = 0;
+    _dWallpaperRequired = 0;
 }
 
 void Room::CalculateArea() {
@@ -83,4 +109,14 @@ void Room::CalculateTilesRequired() {
 
 void Room::CalculateFloorPrice() {
     _dFloorPrice = GetTilePrice() * _dTilesRequired;
+}
+
+void Room::CalculateWallPrice() {
+    double dWallAreaWidth = _dWallHeight * GetWidth();
+    double dWallAreaHeight = _dWallHeight * GetHeight();
+    _dWallArea = dWallAreaHeight + dWallAreaWidth;
+
+    _dWallpaperRequired = _dWallArea / _dWallpaperSize;
+
+    _dWallPrice = _dWallpaperRequired * _dWallpaperCost;
 }
